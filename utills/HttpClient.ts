7e1b125b83373189662205e9fe8d88998be5ce28
quyetlv12 <lequyet840500@gmail.com>
2023-03-06 10:56:25 +0700

    build app to vercel

--- utills/HttpClient.ts
+++ utills/HttpClient.ts
@@ -9,46 +9,46 @@ const HttpClient = Axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL
 });
 
-HttpClient.interceptors.request.use((config: { headers: { Authorization: string; }; isBackgroundRequest: any; }) => {
-    const token = store.getState().app.auth?.authToken
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`
-    }
-    // @ts-ignore
-    if (config.isBackgroundRequest) {
-       return config
-    }
-    store.dispatch(showLoading())
-    return config
-})
+// HttpClient.interceptors.request.use((config: { headers: { Authorization: string; }; isBackgroundRequest: any; }) => {
+//     const token = store.getState().app.auth?.authToken
+//     if (token) {
+//         config.headers.Authorization = `Bearer ${token}`
+//     }
+//     // @ts-ignore
+//     if (config.isBackgroundRequest) {
+//        return config
+//     }
+//     store.dispatch(showLoading())
+//     return config
+// })
 
-HttpClient.interceptors.response.use(
-    (response: { config: { isBackgroundRequest: any; }; }) => {
-        // @ts-ignore
-        if (response && !response.config.isBackgroundRequest) {
-            store.dispatch(hideLoading())
-        }
-        return response
-    },
-    (e) => {
-        store.dispatch(hideLoading())
-        if (!e.response) {
-            return e
-        }
-        switch (e.response.status) {
-            case 400:
-                break;
-            case 403:
-                if (e.response.data.error === 'Forbidden') {
-                    return e.response;
-                }
-                break;
-            case 500:
-                break
-        }
-        store.dispatch(hideLoading())
-        return e.response
-    }
-)
+// HttpClient.interceptors.response.use(
+//     (response: { config: { isBackgroundRequest: any; }; }) => {
+//         // @ts-ignore
+//         if (response && !response.config.isBackgroundRequest) {
+//             store.dispatch(hideLoading())
+//         }
+//         return response
+//     },
+//     (e) => {
+//         store.dispatch(hideLoading())
+//         if (!e.response) {
+//             return e
+//         }
+//         switch (e.response.status) {
+//             case 400:
+//                 break;
+//             case 403:
+//                 if (e.response.data.error === 'Forbidden') {
+//                     return e.response;
+//                 }
+//                 break;
+//             case 500:
+//                 break
+//         }
+//         store.dispatch(hideLoading())
+//         return e.response
+//     }
+// )
 
 export {HttpClient}

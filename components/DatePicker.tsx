f0e69120ce9282a7f5d583029b2adca00b54108d
quyetlv12 <lequyet840500@gmail.com>
2023-03-09 15:23:49 +0700

    update step 3

--- components/DatePicker.tsx
+++ components/DatePicker.tsx
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
 import Calendar from "react-calendar";
+import styled from "styled-components";
+const CalendarStyled= styled.div`
+.react-calendar{
+    width : 100% !important;
+}
+`
 const DatePicker = () => {
   const [value, onChange] = useState(new Date());
 
   return (
-    <div className="mt-4">
+    <CalendarStyled className="mt-4">
       <Calendar onChange={onChange} value={value} />
-    </div>
+    </CalendarStyled>
   );
 };
 
